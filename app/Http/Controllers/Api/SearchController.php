1a6f588abd40b43f16cb3c38d67dd98a36bb7e8a
Your Name <you@example.com>
2025-04-16 10:36:20 +0000

    fix

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -11,22 +11,21 @@ class SearchController extends Controller
     public function searchBrand(Request $request)
     {
         $brands =[];
+        $brands = Brand::select('id', 'name', 'description','image_url','category_id');
         if ($request->filled('keyword')) {
             $search = $request->keyword;
-        
-            $brands = Brand::select('id', 'name', 'description','image_url','category_id')
-                ->where(function ($query) use ($search) {
+            $brands =  $brands->where(function ($query) use ($search) {
                     $query->where('name', 'like', '%' . $search . '%')
                           ->orWhere('description', 'like', '%' . $search . '%')
                           ->orWhere('slug', 'like', '%' . $search . '%');
                 });
-          }
+             }
 
-          if($request->filled('category_id'))
-          {
-            $category_id = $request->category_id;
-            $brands = $brands->where('category_id',$category_id);
-          }
+            if($request->filled('category_id'))
+            {
+                $category_id = $request->category_id;
+                $brands = $brands->where('category_id',$category_id);
+            }
 
          $brands = $brands->paginate(10);// Optional: Limit results for performance
         return response()->json([
